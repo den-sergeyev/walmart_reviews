258d8ee95e91bb8ae8b889ef4f1d755aa7ce94b9
Dzianis Siarheyeu <dzianis_siarheyeu@epam.com>
2016-03-20 20:39:29 +0300

    Prepare for heroku deploy

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+ruby "2.3.0"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
@@ -48,3 +49,9 @@ group :development do
   gem 'spring'
 end
 
+
+group :production do
+  gem 'rails_12factor'
+end
+
+gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160320151246) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.integer  "external_id"
     t.datetime "created_at",  null: false
